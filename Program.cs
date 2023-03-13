3bbc3b017129331a2742b30384b701e6b7ce84ed
Anzstasya <nastya.komleva.2005@mail.ru>
2023-03-13 12:54:52 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Practica1
+{
+    public class Check
+    {
+        public string Nickname { get; set; }
+        public string Breed { get; set; }
+        public float Age { get; set; }
+    }
+}
