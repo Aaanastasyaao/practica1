0d2adc83af925de33f1bb181e851a7339ddc4a74
Anzstasya <nastya.komleva.2005@mail.ru>
2023-03-13 12:55:25 +0300

    second

--- Program.cs
+++ Program.cs
@@ -12,5 +12,20 @@ namespace Practica1
         public string Nickname { get; set; }
         public string Breed { get; set; }
         public float Age { get; set; }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем экземпляр класса Check с помощью конструктора с параметрами
+            Check check = new Check();
+
+            // Выводим информацию о чеке
+            Console.WriteLine("Nickname: " + check.Nickname);
+            Console.WriteLine("Breed: " + check.Breed);
+            Console.WriteLine("Age: " + check.Age);
+
+            // Ожидаем нажатия любой клавиши
+            Console.ReadKey();
+        }
     }
 }
